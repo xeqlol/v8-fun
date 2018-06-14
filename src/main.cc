5de8f53583b5182194dd420bc5e6a1d86c3d87f7
xeqlol <xeqlol@gmail.com>
2018-06-14 14:05:43 +0500

    Added some fun

--- src/main.cc
+++ src/main.cc
@@ -5,8 +5,76 @@
 #include "v8.h"
 #include <iostream>
 
-using namespace v8;
+#include "wtf_snapshot_gen.cc"
 
+using namespace v8;
+/*
+class SnapshotGenerator
+{
+public:
+	SnapshotGenerator(const char *name, const char *filename, v8::StartupData sd) : name_(name), filename_(filename), sd_(sd), file_(filename_, std::ios::binary) {}
+
+	void Write()
+	{
+		file_ << "// Automatically generated file. Do not modify.\n\n";
+		file_ << "#include <v8.h>\n\n";
+
+		char buffer[500];
+
+		snprintf(buffer, sizeof(buffer), "static const char %s_blob_data[] = {\n",
+						 name_);
+		file_ << buffer;
+
+		char buffer2[5];
+		for (int i = 0; i < sd_.raw_size; i++)
+		{
+			if ((i & 0x1F) == 0x1F)
+				file_ << "\n";
+			if (i > 0)
+				file_ << ",";
+			snprintf(buffer2, sizeof(buffer2), "%u",
+							 static_cast<unsigned char>(sd_.data[i]));
+			file_ << buffer2;
+		}
+
+		file_ << "\n";
+		file_ << "};\n";
+		snprintf(buffer, sizeof(buffer), "static const int %s_blob_size = %d;\n",
+						 name_, sd_.raw_size);
+		file_ << buffer;
+		snprintf(buffer, sizeof(buffer), "static const v8::StartupData %s_blob =\n",
+						 name_);
+		file_ << buffer;
+		snprintf(buffer, sizeof(buffer),
+						 "{ (const char*) %s_blob_data, %s_blob_size };\n", name_, name_);
+		file_ << buffer;
+
+		snprintf(buffer, sizeof(buffer), "const v8::StartupData* StartupBlob_%s() {\n", name_);
+		file_ << buffer;
+		snprintf(buffer, sizeof(buffer), "  return &%s_blob;\n", name_);
+		file_ << buffer;
+		file_ << "}\n\n";
+	}
+
+private:
+	const char *name_;
+	const char *filename_;
+	v8::StartupData sd_;
+	std::ofstream file_;
+};
+
+void WriteFile(const char *fn, v8::StartupData startup_data)
+{
+	std::ofstream output(fn, std::ios::binary);
+	output.write(startup_data.data, startup_data.raw_size);
+	output.close();
+	if (output.bad())
+	{
+		printf("Error writing %s\n", fn);
+		exit(1);
+	}
+}
+*/
 int main(int argc, char *argv[])
 {
 	V8::InitializeICUDefaultLocation(argv[0]);
@@ -16,10 +84,19 @@ int main(int argc, char *argv[])
 	V8::InitializePlatform(platform);
 	V8::Initialize();
 
-	const char *source = "function f() {return 42;}";
-	v8::StartupData data = V8::CreateSnapshotDataBlob(source);
-	v8::ArrayBuffer::Allocator *ab_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+	/*const char *source = "function f() {return 42;}";
+	const char *name = "wtf";
+	const char *filename = "wtf_snapshot_gen.cc";
+	SnapshotGenerator *gen = new SnapshotGenerator(name, filename, data);
+	gen->Write();
+	*/
 
+	//const v8::StartupData* fromSnapshot = StartupBlob_wtf();
+	v8::StartupData data = wtf_blob;
+	//data.data = fromSnapshot->data;
+	//data.raw_size = fromSnapshot->raw_size;
+	v8::ArrayBuffer::Allocator *ab_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+	
 	Isolate::CreateParams params;
 	params.array_buffer_allocator = ab_allocator;
 	params.snapshot_blob = &data;
@@ -35,7 +112,7 @@ int main(int argc, char *argv[])
 		Context::Scope context_scope(context);
 
 		Local<String> source =
-				String::NewFromUtf8(isolate, "f()",NewStringType::kNormal).ToLocalChecked();
+			String::NewFromUtf8(isolate, "f()", NewStringType::kNormal).ToLocalChecked();
 
 		Local<Script> script = Script::Compile(context, source).ToLocalChecked();
 		Local<Value> result = script->Run(context).ToLocalChecked();
@@ -48,7 +125,6 @@ int main(int argc, char *argv[])
 	V8::ShutdownPlatform();
 	delete platform;
 	delete params.array_buffer_allocator;
-	delete[] data.data;
 
 	std::cin.get();
 	return 0;
